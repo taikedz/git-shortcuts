493cf5fffdd6611b2645093fe3dc71a892153330
Tai Kedzierski <dch.tai@gmail.com>
2026-07-06 10:51:10 +0100

    Make modules.sh use alphabetical ordering

--- src/modules.sh
+++ src/modules.sh
@@ -2,22 +2,24 @@
 #%include prefs.sh
 #%include store.sh
 
-#%include actions/passthru.sh
-#%include actions/status.sh
-#%include actions/diff.sh
+# ---- Keep the following alphabetical
+
+#%include actions/biggest.sh
+#%include actions/branch.sh
 #%include actions/checkout.sh
+#%include actions/clean.sh
 #%include actions/clone.sh
 #%include actions/commit.sh
+#%include actions/diff.sh
 #%include actions/fetch-prune.sh
+#%include actions/gc.sh
 #%include actions/log.sh
 #%include actions/master.sh
+#%include actions/passthru.sh
 #%include actions/profiles.sh
-#%include actions/push.sh
 #%include actions/pull.sh
-#%include actions/gc.sh
-#%include actions/branch.sh
-#%include actions/clean.sh
-#%include actions/version.sh
+#%include actions/push.sh
 #%include actions/remote.sh
-#%include actions/biggest.sh
 #%include actions/root.sh
+#%include actions/status.sh
+#%include actions/version.sh
